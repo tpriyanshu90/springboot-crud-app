80e7ddbf46fab130e3f57f0d03c0271947c7bf41
Priyanshu Tiwari <tpriyanshu90@gmail.com>
2022-07-09 00:11:50 +0530

    Added Jpa Implementation for accessing database

--- src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package com.luv2code.springboot.cruddemo.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,8 +15,9 @@ public class EmployeeServiceImpl implements EmployeeService {
 
 	private EmployeeDAO employeeDAO;
 	
-	@Autowired
-	public EmployeeServiceImpl(EmployeeDAO theEmployeeDAO) {
+	//Use @Qualifier to specify which bean to select 
+	@Autowired	
+	public EmployeeServiceImpl(@Qualifier("employeeDAOJpaImpl") EmployeeDAO theEmployeeDAO) {
 		employeeDAO = theEmployeeDAO;
 	}
 	
